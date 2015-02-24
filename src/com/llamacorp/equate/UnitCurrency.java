0fbd914fd45d4281a7cbbd03f882e15edc19a740
EvanRespaut <evanrespaut@gmail.com>
2015-02-23 21:53:34 -0500

    Fixed layout bug; margins between smallOpButtons was messed

--- src/com/llamacorp/equate/UnitCurrency.java
+++ src/com/llamacorp/equate/UnitCurrency.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.DateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
